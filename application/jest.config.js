64433bc23742c119a05e6afb0934afdb21f5f327
Tyson Phalp <tyson.phalp@mx.com>
2024-01-17 14:44:46 -0700

    Add Test Coverage (#31)
    Add coverage output folder to gitignore, and add jest coverage

--- application/jest.config.js
+++ application/jest.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  transformIgnorePatterns: ['/node_modules/(?!(@kyper)/).*/'],
+  coverageProvider: 'babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/config/*.js'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'json-summary'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/utils/Test.js',
+    '<rootDir>/src/config/',
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@kyper)/).*/'
+  ],
+  transform: {
+    '^.+\\.[t|j]s?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    "^axios$": "axios/dist/axios.js",
+    // "^@kyper/(.*)$": "@kyper/$1/lib/index.js"
+  }
 }
