045778330fc23bfbe20b29460a667d0209603cee
Tyson Phalp <tyson.phalp@mx.com>
2024-01-17 09:28:07 -0700

    Add all aliases in webpack config

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -23,8 +23,21 @@ module.exports = {
   ],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, 'src'),
+
+      // actions: './src/actions',
+      clients: path.resolve(__dirname, 'src/clients'),
+      components: path.resolve(__dirname, 'src/components'),
+      config: path.resolve(__dirname, 'src/config'),
+      constants: path.resolve(__dirname, 'src/constants'),
+      // css: path.resolve(__dirname, 'src/css'),
+      loggerInit: path.resolve(__dirname, 'loggerInit.js'),
       reduxify: path.resolve(__dirname, 'src/redux'),
+      schemas: path.resolve(__dirname, 'src/schemas'),
+      src: path.resolve(__dirname, 'src'),
+      // stores: path.resolve(__dirname, 'src/stores'),
+      streams: path.resolve(__dirname, 'src/streams'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      widgets: path.resolve(__dirname, 'src/widgets'),
     }
   }
 }
