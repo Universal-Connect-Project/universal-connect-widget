313fd1cf11794d025cab4a141c6560348df15a69
Tyson Phalp <tyson.phalp@mx.com>
2024-02-01 15:40:46 -0700

    Build working....now to test

--- src/index_.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-const App = () => <div>Universal Connect Widget</div>
-
-render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,7 @@
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
-// this file doesn't seem to be in effect, use config-overrides.js instead
-module.exports = {
-  module: {
-    rules: [
-      { test: /\.html$/, use: {loader: 'html?minimize=false'} }
-    ],
-  },
-  plugins: [
-    new NodePolyfillPlugin()
-  ]
-}
+// Used by some third-party tools that requires a webpack.config.js file
+const { paths } = require('react-app-rewired');
+// require normalized overrides
+const overrides = require('react-app-rewired/config-overrides');
+const config = require(paths.scriptVersion + '/config/webpack.config.dev');
+
+module.exports = overrides.webpack(config, process.env.NODE_ENV);
